e9b55b140db13b34abcc58dbc065fc4eb040140a
lavanderhoney <milapp857@gmail.com>
2024-07-04 17:33:18 +0530

    styling fixes to search bar

--- client/src/Components/SearchBar.jsx
+++ client/src/Components/SearchBar.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-
+import '../Styles/SearchBarStyles.css'
 export default function SearchBar(props) {
     return (
-        <div>
-            <form onSubmit={props.getSearch} className='flex flex-row gap-10 px-10 my-8'>
+        <div className='flex justify-center items-center'>
+            <form onSubmit={props.getSearch} className='flex flex-row items-center gap-5 px-10 my-8 border-black border-1 w-[500px]'>
                 <input
-                    className="search-bar border-black border-2 rounded-lg basis-1/2"
+                    className="focus:outline-0 search-bar rounded-lg basis-1/2 bg-slate-300"
                     type="text"
                     value={props.search}
                     onChange={props.updateSearch}
                 />
-                <button className="border-black border-2 rounded-lg" type="submit">
+                <button className="bg-rose-700 text-white rounded-xl p-2 px-3" type="submit">
                     Search
                 </button>
             </form>
