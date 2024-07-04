3843f0709d821943afd6a853aeb263d6b2457330
lavanderhoney <milapp857@gmail.com>
2024-07-04 17:02:35 +0530

    fixing search bar 1

--- client/src/Components/SearchBar.jsx
+++ client/src/Components/SearchBar.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-export default function SearchBar() {
+export default function SearchBar(props) {
     return (
         <div>
-
+            <form onSubmit={props.getSearch} className='flex flex-row gap-10 px-10 my-8'>
+                <input
+                    className="search-bar border-black border-2 rounded-lg basis-1/2"
+                    type="text"
+                    value={props.search}
+                    onChange={props.updateSearch}
+                />
+                <button className="border-black border-2 rounded-lg" type="submit">
+                    Search
+                </button>
+            </form>
         </div>
     )
 }
