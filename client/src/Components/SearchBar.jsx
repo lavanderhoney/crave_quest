da66dd2305f06d979d54caf8963b648c2d1babcd
lavanderhoney <milapp857@gmail.com>
2024-07-05 22:38:53 +0530

    added sidebar, struct changes and install lucide-react

--- client/src/Components/SearchBar.jsx
+++ client/src/Components/SearchBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import '../Styles/SearchBarStyles.css'
 export default function SearchBar(props) {
     return (
-        <div className='flex justify-center items-center'>
+        <div className='flex justify-center'>
             <form onSubmit={props.getSearch} className='flex flex-row items-center gap-5 px-10 my-8 border-black border-1 w-[500px]'>
                 <input
                     className="focus:outline-0 search-bar rounded-lg basis-1/2 bg-slate-300"
