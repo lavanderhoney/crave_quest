88bc022a0c3ad494b0cd92129d0489bbaeee6c9e
lavanderhoney <milapp857@gmail.com>
2024-07-08 23:27:13 +0530

    error handling

--- client/src/Pages/UserMain.jsx
+++ client/src/Pages/UserMain.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 import "../index.css"
 import SearchBar from '../Components/SearchBar';
 
-export default function UserDashboard() {
+export default function UserMain() {
     const { user } = UserAuth();
     const [recipes, setRecipes] = useState([]);
     const [search, setSearch] = useState("");
@@ -22,12 +22,22 @@ export default function UserDashboard() {
             // console.log(response.data.hits);
             // console.log(response.data)
             setRecipes(response.data);
-            // response.data.map((recipe) => {
-            //     console.log("recipe object: ",recipe);
-            //     // console.log(recipe.recipe.ingredientLines);
-            // })
-        } catch (error) {
 
+        } catch (error) {
+            console.log(error);
+            const recipeError = [{
+                recipe:
+                {
+                    label: "Not Found",
+                    image: "",
+                    ingredientLines: error.message
+                }
+
+            }]
+            console.log("eror obj:", recipeError);
+            setRecipes(recipeError);
+            console.log(recipes);
+            console.log("[0]: ",recipes[0].recipe);
         }
 
     };
@@ -44,20 +54,26 @@ export default function UserDashboard() {
 
 
     return (
-        <div>
+        <div className='w-full'>
             <div>
                 <h1 className='font-serif m-2 my-4 text-4xl '>Welcome {user?.displayName}, find the recipe you crave now !</h1>
                 <SearchBar getSearch={getSearch} search={search} updateSearch={updateSearch} />
+                {recipes[0].recipe.label === 'Not Found' ? (
+                            <div className='flex flex-col justify-center'>
+                                <div  className="text-center p-5 text-red-700 font-bold text-2xl">Sorry, we couldn't find any recipes.</div>
+                                <span className="text-center p-5 text-red-600 font-semibold text-xl">{recipes[0].recipe.ingredientLines}</span>
+                            </div>
+                ):
                 <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
                     {recipes.map((recipe, index) => (
-                        <RecipeCard2
-                            key={index}
-                            title={recipe.recipe.label}
-                            image={recipe.recipe.image}
-                            ingredients={recipe.recipe.ingredientLines}
-                        />
+                            <RecipeCard2
+                                key={index}
+                                title={recipe.recipe.label}
+                                image={recipe.recipe.image}
+                                ingredients={recipe.recipe.ingredientLines}
+                            />
                     ))}
-                </div>
+                </div>}
 
                 {/* <RecipeCard2></RecipeCard2> */}
             </div>
